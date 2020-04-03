16c54bcf06a5090d7aea97fde39bde49add68eda
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-04-03 02:43:29 -0300

    VERDE - copia 2537 como MMDXXXVII

--- src/romanos/EscribaRomano.java
+++ src/romanos/EscribaRomano.java
@@ -2,8 +2,8 @@ package romanos;
 
 public class EscribaRomano {
 
-	private String[] simbolos = {"C", "L", "XL", "X", "IX", "V", "IV", "I"};
-	private int[] valores = {100, 50, 40, 10, 9, 5, 4, 1};
+	private String[] simbolos = {"M", "D", "C", "L", "XL", "X", "IX", "V", "IV", "I"};
+	private int[] valores = {1000, 500, 100, 50, 40, 10, 9, 5, 4, 1};
 	
 	public String copiar(int numero) {
 
