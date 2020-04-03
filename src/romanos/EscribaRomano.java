a417acc778fd19292b680776bff27f7d16be0ea2
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-04-03 02:27:29 -0300

    REFACTOR - Reemplaza secuencia de if por la iteración por arreglos

--- src/romanos/EscribaRomano.java
+++ src/romanos/EscribaRomano.java
@@ -2,38 +2,19 @@ package romanos;
 
 public class EscribaRomano {
 
+	private String[] simbolos = {"X", "V", "IV", "I"};
+	private int[] valores = {10, 5, 4, 1};
+	
 	public String copiar(int numero) {
 
 		String romano = "";
 		
-		if (numero >= 10) {
-		
-			romano = romano + concatenar("X", numero / 10);
-
-			numero = numero % 10;
-		} 
-		
-		if (numero >= 5) {
+		for (int i = 0; i < simbolos.length; i++) {
 			
-			romano = romano + concatenar("V", numero / 5);
-			
-			numero = numero % 5;
+			romano = romano + concatenar(simbolos[i], numero / valores[i]);
+			numero = numero % valores[i];
 		}
 		
-		if (numero >= 4) {
-			
-			romano = romano + concatenar("IV", numero / 4);
-
-			numero = numero % 4; 
-		} 
-		
-		if (numero >= 1) {
-
-			romano = romano + concatenar("I", numero / 1);
-			
-			numero = numero % 1;
-		}
-
 		return romano;
 	}
 	
