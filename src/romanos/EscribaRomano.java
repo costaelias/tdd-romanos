50fd0cf4a5205f89f94e8cf8212fe624e669267c
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-04-04 01:27:27 -0300

    REFACTOR - Documenta la interfaz

--- src/romanos/EscribaRomano.java
+++ src/romanos/EscribaRomano.java
@@ -5,6 +5,10 @@ public class EscribaRomano {
 	private String[] simbolos = {  "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 	private int[]     valores = { 1000,  900, 500,  400, 100,   90,  50,   40,  10,    9,   5,    4,   1 };
 	
+	/**
+	 * @pre 'numero' es un valor mayor a 0.
+	 * @post traduce el 'numero' a notación romana y la devuelve.
+	 */
 	public String copiar(int numero) {
 
 		validarQueSeaCopiable(numero);
